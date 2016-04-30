6bf5d1bbe4f05fa1ddd71d2b48d02c353e356d45
Mutti <mutti.appsimples@gmail.com>
2016-04-30 20:19:41 -0300

    commit exemplo

---
+++ app/src/main/java/com/example/mutti/interusp_android/Selecionar_Atletica.java
@@ -0,0 +1,13 @@
+package com.example.mutti.interusp_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Selecionar_Atletica extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selecionar__atletica);
+    }
+}
--- app/src/main/java/com/example/mutti/interusp_android/Splash.java
+++ app/src/main/java/com/example/mutti/interusp_android/Splash.java
@@ -1,13 +1,37 @@
 package com.example.mutti.interusp_android;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Splash extends AppCompatActivity {
 
+    Activity activity = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+
+
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+
+                    activity.finish();
+                    Intent intent1 = new Intent(activity, Selecionar_Atletica.class);
+                    startActivity(intent1);
+
+            }
+        }, 3000);
+
+
     }
 }
