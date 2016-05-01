f85f9b9fb2b798a93a2d1768c66d1f445aee8781
Wendler Eis <wendler_eis@hotmail.com>
2016-05-01 09:19:25 -0300

    Tela - Atulaizar Colocaçao

---
+++ app/src/main/java/com/example/mutti/interusp_android/atualizar_colocacao_jogos.java
@@ -0,0 +1,13 @@
+package com.example.mutti.interusp_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class atualizar_colocacao_jogos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atualizar_colocacao_jogos);
+    }
+}
