db6a6d626a5617b53cf2874c04f95e53b3a2be3d
Rafa <rafakamada@gmail.com>
2016-05-01 07:36:28 -0300

    commit tela hinos

---
+++ app/src/main/java/com/example/mutti/interusp_android/Gritos.java
@@ -0,0 +1,201 @@
+package com.example.mutti.interusp_android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+
+
+public class Gritos extends AppCompatActivity {
+    Activity activity = this;
+    private boolean a = true;
+    private boolean b = true;
+    private boolean c = true;
+    private boolean d = true;
+    ImageView botao;
+    ScrollView scroll;
+
+    public void mudaTexto(boolean k, TextView titulo, TextView texto, int x){
+        if (k){
+            texto.setMaxLines(100);
+            if(x==1)
+                this.a = false;
+            if(x==2) {
+                this.b = false;
+                if (c && d){
+                    scroll.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            scroll.fullScroll(View.FOCUS_DOWN);
+                        }
+            });}}
+            if(x==3) {
+                this.c = false;
+                if (d){
+                    scroll.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            scroll.fullScroll(View.FOCUS_DOWN);
+                        }
+                    });}
+            }
+            if(x==4) {
+                this.d = false;
+                scroll.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        scroll.fullScroll(View.FOCUS_DOWN);
+                    }
+                });
+            }
+
+        } else{
+            texto.setMaxLines(0);
+            if(x==1)
+                this.a = true;
+            if(x==2)
+                this.b = true;
+            if(x==3)
+                this.c = true;
+            if(x==4)
+                this.d = true;
+        }
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gritos2);
+
+        String atletica = getIntent().getStringExtra("atletica");
+
+        final TextView titulo1 = (TextView) findViewById(R.id.textView);
+        final TextView texto1 = (TextView) findViewById(R.id.textView3);
+        final TextView titulo2 = (TextView) findViewById(R.id.textView4);
+        final TextView texto2 = (TextView) findViewById(R.id.textView5);
+        final TextView titulo3 = (TextView) findViewById(R.id.textView6);
+        final TextView texto3 = (TextView) findViewById(R.id.textView7);
+        final TextView titulo4 = (TextView) findViewById(R.id.textView8);
+        final TextView texto4 = (TextView) findViewById(R.id.textView9);
+        Selecionar_Atletica selecionar = new Selecionar_Atletica();
+        scroll = (ScrollView) findViewById(R.id.scrollView);
+        botao = (ImageView) findViewById(R.id.button50);
+        switch (atletica){
+            case "poli":
+                titulo1.setText(getResources().getString(R.string.titulo1Poli));
+                titulo2.setText(getResources().getString(R.string.titulo2Poli));
+                titulo3.setText(getResources().getString(R.string.titulo3Poli));
+                titulo4.setText(getResources().getString(R.string.titulo4Poli));
+                texto1.setText(getResources().getString(R.string.grito1Poli));
+                texto2.setText(getResources().getString(R.string.grito2Poli));
+                texto3.setText(getResources().getString(R.string.grito3Poli));
+                texto4.setText(getResources().getString(R.string.grito4Poli));
+                break;
+            case "fea":
+                titulo1.setText(getResources().getString(R.string.titulo1Fea));
+                titulo2.setText(getResources().getString(R.string.titulo2Fea));
+                titulo3.setText(getResources().getString(R.string.titulo3Fea));
+                titulo4.setText(getResources().getString(R.string.titulo4Fea));
+                texto1.setText(getResources().getString(R.string.texto1Fea));
+                texto2.setText(getResources().getString(R.string.texto2Fea));
+                texto3.setText(getResources().getString(R.string.texto3Fea));
+                texto4.setText(getResources().getString(R.string.texto4Fea));
+               botao.setImageResource(R.drawable.logo_iusp_splash);
+
+                break;
+            case "esalq":
+                titulo1.setText((getResources().getString(R.string.titulo1Esalq)));
+                titulo2.setText((getResources().getString(R.string.titulo2Esalq)));
+                titulo3.setText((getResources().getString(R.string.titulo3Esalq)));
+                titulo4.setText((getResources().getString(R.string.titulo4Esalq)));
+                texto1.setText((getResources().getString(R.string.texto1Esalq)));
+                texto2.setText((getResources().getString(R.string.texto2Esalq)));
+                texto3.setText((getResources().getString(R.string.texto3Esalq)));
+                texto4.setText((getResources().getString(R.string.texto4Esalq)));
+                break;
+            case "odonto":
+                titulo1.setText((getResources().getString(R.string.titulo1Odonto)));
+                titulo2.setText((getResources().getString(R.string.titulo2Odonto)));
+                titulo3.setText((getResources().getString(R.string.titulo3Odonto)));
+                titulo4.setText((getResources().getString(R.string.titulo4Odonto)));
+                texto1.setText((getResources().getString(R.string.texto1Odonto)));
+                texto2.setText((getResources().getString(R.string.texto2Odonto)));
+                texto3.setText((getResources().getString(R.string.texto3Odonto)));
+                texto4.setText((getResources().getString(R.string.texto4Odonto)));
+                break;
+            case "farma":
+                titulo1.setText((getResources().getString(R.string.titulo1Farma)));
+                titulo2.setText((getResources().getString(R.string.titulo2Farma)));
+                titulo3.setText((getResources().getString(R.string.titulo3Farma)));
+                titulo4.setText((getResources().getString(R.string.titulo4Farma)));
+                texto1.setText((getResources().getString(R.string.texto1Farma)));
+                texto2.setText((getResources().getString(R.string.texto2Farma)));
+                texto3.setText((getResources().getString(R.string.texto3Farma)));
+                texto4.setText((getResources().getString(R.string.texto4Farma)));
+                break;
+            case "pinheiros":
+                titulo1.setText((getResources().getString(R.string.titulo1Pinheiros)));
+                titulo2.setText((getResources().getString(R.string.titulo2Pinheiros)));
+                titulo3.setText((getResources().getString(R.string.titulo3Pinheiros)));
+                titulo4.setText((getResources().getString(R.string.titulo4Pinheiros)));
+                texto1.setText((getResources().getString(R.string.texto1Pinheiros)));
+                texto2.setText((getResources().getString(R.string.texto2Pinheiros)));
+                texto3.setText((getResources().getString(R.string.texto3Pinheiros)));
+                texto4.setText((getResources().getString(R.string.texto4Pinheiros)));
+                break;
+            case "ribeirao":
+                titulo1.setText((getResources().getString(R.string.titulo1Ribeirao)));
+                titulo2.setText((getResources().getString(R.string.titulo2Ribeirao)));
+                titulo3.setText((getResources().getString(R.string.titulo3Ribeirao)));
+                titulo4.setText((getResources().getString(R.string.titulo4Ribeirao)));
+                texto1.setText((getResources().getString(R.string.texto1Ribeirao)));
+                texto2.setText((getResources().getString(R.string.texto2Ribeirao)));
+                texto3.setText((getResources().getString(R.string.texto3Ribeirao)));
+                texto4.setText((getResources().getString(R.string.texto4Ribeirao)));
+                break;
+            case "sanfran":
+                titulo1.setText((getResources().getString(R.string.titulo1Sanfran)));
+                titulo2.setText((getResources().getString(R.string.titulo2Sanfran)));
+                titulo3.setText((getResources().getString(R.string.titulo3Sanfran)));
+                titulo4.setText((getResources().getString(R.string.titulo4Sanfran)));
+                texto1.setText((getResources().getString(R.string.texto1Sanfran)));
+                texto2.setText((getResources().getString(R.string.texto2Sanfran)));
+                texto3.setText((getResources().getString(R.string.texto3Sanfran)));
+                texto4.setText((getResources().getString(R.string.texto4Sanfran)));
+                break;
+        }
+
+        titulo1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                    mudaTexto(a, titulo1, texto1, 1);
+                }
+            });
+        titulo2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mudaTexto(b, titulo2, texto2, 2);
+            }
+        });
+        titulo3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mudaTexto(c, titulo3, texto3, 3);
+            }
+        });
+        titulo4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mudaTexto(d, titulo4, texto4, 4);
+            }
+        });
+
+    }
+
+
+}
--- app/src/main/java/com/example/mutti/interusp_android/Selecionar_Atletica.java
+++ app/src/main/java/com/example/mutti/interusp_android/Selecionar_Atletica.java
@@ -1,13 +1,103 @@
 package com.example.mutti.interusp_android;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
+
+import java.util.ArrayList;
 
 public class Selecionar_Atletica extends AppCompatActivity {
+    public int faculdade;
+    Activity activity = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selecionar__atletica);
+
+
+        Button botaoAtleticaPoli = (Button) findViewById(R.id.botaoPoli);
+        Button botaoAtleticaFea = (Button) findViewById(R.id.botaoFea);
+        Button botaoAtleticaEsalq = (Button) findViewById(R.id.botaoEsalq);
+        Button botaoAtleticaOdonto = (Button) findViewById(R.id.botaoOdonto);
+        Button botaoAtleticaFarma = (Button) findViewById(R.id.botaoFarma);
+        Button botaoAtleticaPinheiros = (Button) findViewById(R.id.botaoPinheiros);
+        Button botaoAtleticaRibeirao = (Button) findViewById(R.id.botaoRibeirao);
+        Button botaoAtleticaSanfran = (Button) findViewById(R.id.botaoSanfran);
+
+
+
+        botaoAtleticaPoli.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity,Gritos.class );
+                intent.putExtra("atletica", "poli");
+                startActivity(intent);
+            }
+        });
+
+        botaoAtleticaFea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity,Gritos.class );
+                intent.putExtra("atletica", "fea");
+                startActivity(intent);
+            }
+        });
+        botaoAtleticaEsalq.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity,Gritos.class );
+                intent.putExtra("atletica", "esalq");
+                startActivity(intent);
+            }
+        });
+        botaoAtleticaOdonto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity,Gritos.class );
+                intent.putExtra("atletica", "odonto");
+                startActivity(intent);
+            }
+        });
+        botaoAtleticaFarma.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity,Gritos.class );
+                intent.putExtra("atletica", "farma");
+                startActivity(intent);
+            }
+        });
+        botaoAtleticaPinheiros.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity,Gritos.class );
+                intent.putExtra("atletica", "pinheiros");
+                startActivity(intent);
+            }
+        });
+        botaoAtleticaRibeirao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity,Gritos.class );
+                intent.putExtra("atletica", "ribeirao");
+                startActivity(intent);
+            }
+        });
+        botaoAtleticaSanfran.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(activity,Gritos.class );
+                intent.putExtra("atletica", "sanfran");
+                startActivity(intent);
+            }
+        });
+
+
     }
+
 }
